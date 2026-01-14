75f1995e70c94cd30466a1e583e701db6877db05
Navas Shareef <navasshareef188@gmail.com>
2026-01-14 14:34:41 +0530

    Clean production branch

--- lib/core/asset_constants.dart
+++
@@ -1,8 +0,0 @@
-class AssetConstants {
-  static const profile = 'assets/images/profile.webp';
-  static const genskill = 'assets/images/genskill.webp';
-  static const necqatar = 'assets/images/necqatar.webp';
-  static const gulfexchange = 'assets/images/gulfexchange.webp';
-  static const torus = 'assets/images/torus.webp';
-  static const wallstreet = 'assets/images/wallstreet.webp';
-}
